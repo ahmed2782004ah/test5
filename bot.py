c84414e249e4d41b42097dfdd57d2de56d17003c
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-21 13:47:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ bot = Client(
     "mo",
     api_id=8186557,
     api_hash="efd77b34c69c164ce158037ff5a0d117",
-    bot_token="5912908363:AAGkAqH3VTR0d-g0lAervmElqo9TqgDYLu4",#توكن المصنع
+    bot_token="6995478122:AAEmyOmOvRTg45OjfKjre6wEkuRyWOrbuuI",#توكن المصنع
     plugins=dict(root="MHelal")
     )
 
 async def start_helalbot():
     print("تم تشغيل الصانع بنجاح..💗")
     await bot.start()
-    hh = "MohamedHelal_l"#يوزر المطور المصنع
+    hh = "IIUll_l"#يوزر المطور المصنع
     try:
         await bot.send_message(hh, "**تم تشغيل الصانع بنجاح ...🥀**")
     except:
